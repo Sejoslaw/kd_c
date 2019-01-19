74d13496a84062fdc0a79470d29dd3ed935bfeec
Krzysztof Dobrzyński <k.dobrzynski94@gmail.com>
2019-01-19 11:12:50 +0100

    Added initial version of kd.h;

---
+++ Tests/test.c
@@ -0,0 +1,8 @@
+// This must be define only ONCE.
+#define KD_DEFINE
+#include "../kd.h"
+
+int main(int argc, char *argv[]) {
+    printf("Hello World !!!");
+    return 0;
+}
---
+++ kd.h
@@ -0,0 +1,63 @@
+/* kd.h - v1.0.0 - Krzysztof Dobrzynski's main library -- https://github.com/Sejoslaw/kd_c
+
+    No warranty is offered or implied; use this code at your own risk !!!
+    Have fun ^^
+
+    This is a single header file for getting stuff done in C/C++.
+
+    ====================================================================
+    Before you start doing anything you must define:
+
+    #define KD_DEFINE
+    #include "kd.h"
+
+    In other files you must just include "kd.h" without the #define.
+    ====================================================================
+
+    For the license please see: https://github.com/Sejoslaw/kd_c/blob/master/LICENSE
+
+    Versions:
+        1.0.0 - Initial version
+*/
+
+#ifdef KD_DEFINE
+
+
+/* 
+    Include standard C libraries.
+
+    Full list of standard C libraries can be found here: https://en.wikipedia.org/wiki/C_standard_library
+*/
+#include <assert.h>
+#include <complex.h>
+#include <ctype.h>
+#include <errno.h>
+#include <fenv.h>
+#include <float.h>
+#include <inttypes.h>
+#include <iso646.h>
+#include <limits.h>
+#include <locale.h>
+#include <math.h>
+#include <setjmp.h>
+#include <signal.h>
+// #include <stdalign.h>
+#include <stdarg.h>
+// #include <stdatomic.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+// #include <stdnoreturn.h>
+#include <string.h>
+// #include <tgmath.h>
+// #include <threads.h>
+// #include <pthread.h>
+#include <time.h>
+#include <uchar.h>
+#include <wchar.h>
+#include <wctype.h>
+
+
+#endif // KD_DEFINE
